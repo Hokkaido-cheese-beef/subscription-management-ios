eae805933b74e604b1a46e32989fbeab68b6cdf4
Kaito Kudou <kaitokudou2468110921@gmail.com>
2022-04-26 22:36:56 +0900

    [fix]初期画面をHome用画面に変更

--- subscription-management-ios/subscription-management-ios/subscription_management_iosApp.swift
+++ subscription-management-ios/subscription-management-ios/subscription_management_iosApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct subscription_management_iosApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            HomeView()
         }
     }
 }

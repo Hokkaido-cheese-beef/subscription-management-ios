538fecc77f79baf9e83fdea6abbcf492d1989e4e
Kaito Kudou <kaitokudou2468110921@gmail.com>
2022-04-26 22:41:53 +0900

    [add]ファイル追加

---
+++ subscription-management-ios/subscription-management-ios/View/AddSubscription/AddSubscriptionView.swift
@@ -0,0 +1,20 @@
+//
+//  AddSubscriptionView.swift
+//  subscription-management-ios
+//
+//  Created by 工藤 海斗 on 2022/04/26.
+//
+
+import SwiftUI
+
+struct AddSubscriptionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddSubscriptionView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddSubscriptionView()
+    }
+}

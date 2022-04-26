cfc2604edacb4cc460c96fe963e399b55a7603a6
Kaito Kudou <kaitokudou2468110921@gmail.com>
2022-04-26 22:42:11 +0900

    [add]カレンダー同期ボタンを作成

---
+++ subscription-management-ios/subscription-management-ios/View/Home/CalendarSyncButton.swift
@@ -0,0 +1,29 @@
+//
+//  CalendarSyncButton.swift
+//  subscription-management-ios
+//
+//  Created by 工藤 海斗 on 2022/04/26.
+//
+
+import SwiftUI
+
+struct CalendarSyncButton: View {
+    var body: some View {
+        Button {
+            print("カレンダー同期ボタンが押された")
+        } label: {
+            Text("カレンダー同期")
+                .frame(width: UIScreen.main.bounds.width - 160, height: 40, alignment: .center)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.secondary, lineWidth: 2)
+                )
+        }
+    }
+}
+
+struct CalendarSyncButton_Previews: PreviewProvider {
+    static var previews: some View {
+        CalendarSyncButton()
+    }
+}

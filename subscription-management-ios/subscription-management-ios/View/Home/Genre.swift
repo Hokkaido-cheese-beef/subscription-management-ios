6f875c855759459715a4f76ce98170cecc6148ce
Kaito Kudou <kaitokudou2468110921@gmail.com>
2022-07-06 01:20:07 +0900

    [add]ジャンルを定義

--- subscription-management-ios/subscription-management-ios/View/Home/Genre.swift
+++ subscription-management-ios/subscription-management-ios/View/Home/Genre.swift
@@ -11,6 +11,11 @@ import Foundation
 enum Genre: CaseIterable {
     case music
     case movie
+    case storage
+    case book
+    case business
+    case fashion
+    case workcation
     
     var genreName: String {
         switch self {
@@ -18,6 +23,16 @@ enum Genre: CaseIterable {
             return "音楽"
         case .movie:
             return "動画"
+        case .storage:
+            return "ストレージ"
+        case .book:
+            return "本・漫画・雑誌"
+        case .business:
+            return "ビジネス"
+        case .fashion:
+            return "ファッション"
+        case .workcation:
+            return "ワーケーション"
         }
     }
 }
